049a0eeeb66cdb726834afab944f71a4b53fd78a
cyraalesha <67356675+cyraalesha@users.noreply.github.com>
2020-12-30 14:48:36 +0700

    taildown dependency downgrade andd fixing up a mess

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
         <h1> TODO </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=TODO"> Jakarta </a>
+        <a href="/?city=TODO"> New York </a>
+        <a href="/?city=TODO"> Bandung </a>
+        <a href="/?city=TODO"> London </a>
       </nav>
     </header>
   );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,16 +4,12 @@ import React, { useState, useEffect, useMemo } from "react";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
-<<<<<<< HEAD
 import City from "../components/City";
-=======
->>>>>>> parent of 47097d02... city functions and list
 
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
-<<<<<<< HEAD
   return (
     <div className="flex flex-col h-screen bg-green-200">
       <City cityName={"Tokyo"} temp={"10°C"} color="bg-yellow-500" />
@@ -21,8 +17,6 @@ function Home() {
       <City cityName={"Bandung"} temp={"17°C"} color="bg-blue-500" />
     </div>
   );
-=======
->>>>>>> parent of 47097d02... city functions and list
 }
 
 export default Home;
